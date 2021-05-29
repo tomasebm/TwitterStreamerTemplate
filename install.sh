e94e69ec63038fc84a0dc302bc76f412e43c8daf
Tom <tomasmaguire@gmail.com>
2021-05-29 20:32:54 -0300

    upd

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt install python3-pip -y
 sudo apt install htop -y
+sudo apt install mailutils -y
 sudo pip3 install tweepy
 sudo pip3 install dataset
 sudo pip3 install pandas
--- scripts/diskusagewarning.sh
+++ scripts/diskusagewarning.sh
@@ -4,10 +4,10 @@
 # script that will send an email to EMAIL when disk use in partition PART is bigger than %MAX
 # adapt these 3 parameters to your case
 MAX=90
-EMAIL=laiadomenechburin@gmail.com
+EMAIL=ejemplo@gmail.com
 PART=xvda1
 
 USE=`df -h |grep $PART | awk '{ print $5 }' | cut -d'%' -f1`
 if [ $USE -gt $MAX ]; then
-  echo "Porcentaje en uso: $USE" | mail -s "Scraper de Twitter (Mercado cambiario/Ariel Wilkis) con poco espacio restante." $EMAIL
+  echo "Porcentaje en uso: $USE" | mail -s "Scraper de Twitter con poco espacio restante." $EMAIL
 fi
